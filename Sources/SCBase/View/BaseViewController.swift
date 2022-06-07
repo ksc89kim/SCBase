cc517d9d8cbcb3714e41f180d8b2de07767e4f7c
ksc89kim <ksc89kim@naver.com>
2022-06-07 15:25:51 +0900

    BaseViewController ViewLifeCycle 프로토콜 제거

--- Sources/SCBase/View/BaseViewController.swift
+++ Sources/SCBase/View/BaseViewController.swift
@@ -10,38 +10,18 @@
 
 import UIKit
 
-open class BaseViewController: UIViewController, ViewLifeCycle, MakeLayout {
+open class BaseViewController: UIViewController, MakeLayout {
 
   // MARK: - Property
 
   open var didMakeConstraints: Bool = false
 
-  // MARK: - Init
-
-  public init() {
-    defer {
-      self.viewDidInit()
-      self.makeLayout()
-    }
-    super.init(nibName: nil, bundle: nil)
-  }
-
-  public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    defer {
-      self.viewDidInit()
-      self.makeLayout()
-    }
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-  }
-
-  required public init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
   // MARK: - Life Cycle
 
-  open func viewDidInit() {
-    // override point
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.makeLayout()
   }
 
   // MARK: - Layout
